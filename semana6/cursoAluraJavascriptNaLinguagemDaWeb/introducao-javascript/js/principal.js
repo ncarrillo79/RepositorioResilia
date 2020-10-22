6fef7705985bf809923ff23afa997ceae366eac0
Norma <ncarrillo79@hotmail.com>
2020-10-22 17:09:08 -0300

    agregado excercicio aula 27

---
+++ semana6/cursoAluraJavascriptNaLinguagemDaWeb/introducao-javascript/js/principal.js
@@ -0,0 +1,8 @@
+console.log('Fui carregado de um arquivo externo');
+
+var titulo = document.querySelector('.titulo');
+
+titulo.textContent = 'Aparecida Nutricionista';
+
+var paciente = document.querySelector('#primeiro-paciente');
+console.log(paciente);
